0a3eb54df2c6ef9d3170facaba1bed8f9a4b7060
Alexis <edwinalexis1996@gmail.com>
2020-06-02 20:07:33 -0500

    Updates

---
+++ precache-manifest.4c27934cfba38578c1b1f581a274a33b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55432757f9a33ee4d329973d7976fedb",
+    "url": "/news-room/index.html"
+  },
+  {
+    "revision": "46afe828119271115b68",
+    "url": "/news-room/static/css/main.646f12ac.chunk.css"
+  },
+  {
+    "revision": "3f387dd8b0de645f4e99",
+    "url": "/news-room/static/js/2.28df7e1c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/news-room/static/js/2.28df7e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46afe828119271115b68",
+    "url": "/news-room/static/js/main.f7fee231.chunk.js"
+  },
+  {
+    "revision": "88df5dcb64de81dfbdfa",
+    "url": "/news-room/static/js/runtime-main.812dc4ec.js"
+  }
+]);
